b5a9d7c5d59bb8e637f8f694005ac6e9be261ac0
elisagomes3 <161314898+elisagomes3@users.noreply.github.com>
2024-04-15 09:31:10 -0300

    Add files via upload

---
+++ atvsJS/js2.js
@@ -0,0 +1,11 @@
+function contagemRegressiva (segundos) {
+    for (let i = segundos; i>= 0; i--)}
+    setTimeout(function(){
+        if (i===0){
+
+console.log("Lançamento!");
+        } else {
+            console.log("Contagem regressiva:" + i);
+        }
+        } (segundos - i) * 1000);
+    
